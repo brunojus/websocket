b607e94f286dcf4c9f89c148e0272c99abcd5d53
Bruno Justino <bruno_justino@icloud.com>
2017-12-01 21:02:02 -0200

    Trying to do http requests

--- proxy_cache.c
+++ proxy_cache.c
@@ -15,8 +15,87 @@ struct addrinfo hints,*serv_info,host_addr;
 struct sockaddr_storage their_addr;
 FILE* fd;
 
+
+void request(int newsockfd)
+{
+
+			 char s[INET6_ADDRSTRLEN];
+			 char buffer[1024];
+
+			 int flag=0,n,i;
+			 char t1[512],t2[512],t3[16];
+			 char* temporary=NULL;
+			 char* port;
+			 bzero(buffer,1024);
+			 recv(newsockfd,buffer,1024,0);
+			 sscanf(buffer,"%s %s %s",t1,t2,t3);
+
+			 printf("%s\n\n\n\n\n\n\n\n\n", t1);
+			 printf("%s\n\n\n\n\n\n\n\n\n", t2);
+			 printf("%s\n\n\n\n\n\n\n\n\n", t3);
+
+			 if(((strncmp(t1,"GET",3)==0))&&((strncmp(t3,"HTTP/1.1",8)==0)||(strncmp(t3,"HTTP/1.0",8)==0))&&(strncmp(t2,"http://",7)==0))
+			 {
+					 strcpy(t1,t2);
+					 flag=0;
+					 for(i=7;i<strlen(t2);i++)
+					 {
+							 if(t2[i]==':')
+							 {
+									 flag=1;
+									 break;
+							 }
+				 }
+					 temporary=strtok(t2,"//");
+
+					 if(flag==0)
+					 {
+							 port="80";
+							 temporary=strtok(NULL,"/");
+					 }
+					 else
+					 {
+							 temporary=strtok(NULL,":");
+					 }
+					 sprintf(t2,"%s",temporary);
+
+							 printf("host = %s",t2);
+
+					 if(flag==1)
+					 {
+							 temporary=strtok(NULL,"/");
+							 port=temporary;
+					 }
+
+					 strcat(t1,"^]");
+					 temporary=strtok(t1,"//");
+					 temporary=strtok(NULL,"/");
+
+					 if(temporary!=NULL)
+							 temporary=strtok(NULL,"^]");
+							 printf("\npath = %s\nPort = %s\n",temporary,port);
+							 i = directory(t2,temporary);
+
+							 if(i==1)
+							 {
+									 printf("From Host\n");
+									 connect_host(t2,port,temporary,t3,newsockfd);
+							 }
+							 else
+							 {
+									 printf("From cache\n");
+									 send_file(t2,port,temporary,t3,newsockfd);
+					 }
+	 }
+
+			 else
+			 {
+					 send(newsockfd,"400 : BAD REQUEST\nONLY HTTP REQUESTS ALLOWED",44,0);
+			 }
+
+}
 int start_server(int sockfd)
-	{
+{
 		printf("Server Start Running.........\n");
 		char s[INET6_ADDRSTRLEN];
 		socklen_t sin_size;
@@ -43,7 +122,7 @@ int start_server(int sockfd)
 				break;
 		}
 		return newsockfd;
-	}
+}
 
 
 int directory(char* host,char *path)
@@ -84,7 +163,7 @@ int directory(char* host,char *path)
 		}
 
 
-		arq = fopen("Homepage","a+");
+		fd = fopen("Homepage","a+");
 	}
 	else
 	{
@@ -94,7 +173,7 @@ int directory(char* host,char *path)
 		}
 
 
-	arq = fopen(extension,"a+");
+	fd = fopen(extension,"a+");
 	}
 	return flag;
 }
